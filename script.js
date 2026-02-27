613a8af6ac617a3ade1966aed825c1eef8807b2c
Daniel Nowak <danieln311@gmail.com>
2026-02-27 13:05:18 +0000

    Create event delegation to gridContainer, listening to events that match class IN the container.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const gridContainer = document.querySelector("#gridContainer")
 const button = document.querySelector(".button")
-const buttonPink = document.querySelector(".buttonPink")
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("square")) {
+        e.target.style.backgroundColor = "orange";
+    }
+})
 
 button.addEventListener("click", createGrid);
-buttonPink.addEventListener("click", createPinkGrid);
+
+
 
 function createGrid() {
     gridContainer.innerHTML = "";
@@ -15,17 +21,12 @@ function createGrid() {
         newDiv.classList.add("square");
         gridContainer.appendChild(newDiv);
     }
-}
+    const square = document.querySelector(".square");
+};
 
-function createPinkGrid() {
-    gridContainer.innerHTML = "";
 
-    const size = 16;
 
-    for (let i = 0; i < size * size; i++) {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("squarePink");
-        gridContainer.appendChild(newDiv);
-    }
-}
+
+
+
 

d94e95f6ecf973df24d5d65420278f4ce7324317
Daniel Nowak <danieln311@gmail.com>
2026-02-27 11:22:03 +0000

    Create duplicate function to create grid but in pink, for testing.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const gridContainer = document.querySelector("#gridContainer")
 const button = document.querySelector(".button")
+const buttonPink = document.querySelector(".buttonPink")
 
 button.addEventListener("click", createGrid);
+buttonPink.addEventListener("click", createPinkGrid);
 
 function createGrid() {
     gridContainer.innerHTML = "";
@@ -11,6 +13,18 @@ function createGrid() {
     for (let i = 0; i < size * size; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("square");
+        gridContainer.appendChild(newDiv);
+    }
+}
+
+function createPinkGrid() {
+    gridContainer.innerHTML = "";
+
+    const size = 16;
+
+    for (let i = 0; i < size * size; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("squarePink");
         gridContainer.appendChild(newDiv);
     }
 }

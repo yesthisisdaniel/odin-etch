21cc9cdddb1000a32fced6501165957d1e164276
Daniel Nowak <danieln311@gmail.com>
2026-03-05 14:25:28 +0000

    Add eraser to drawing switch board + change clear button text.

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const gridContainer = document.querySelector("#gridContainer")
-const gridButton = document.querySelector("#gridButton")
-const rainbowButton = document.querySelector("#rainbowColor")
-const blackButton = document.querySelector("#blackColor")
+const gridContainer = document.querySelector("#gridContainer");
+const gridButton = document.querySelector("#gridButton");
+const rainbowButton = document.querySelector("#rainbowColor");
+const blackButton = document.querySelector("#blackColor");
+const clearButton = document.querySelector("#clearButton");
 
 
-let isDrawing = false;
-let drawingColor = "black"; 
+let isDrawing = false; // needs to be declared once and on top of code for global scope
+let drawingColor = "black"; // default will always be black
 
-function randColorNum() {
+function randomColorNum() {
     return Math.floor(Math.random() * 256);
 }
 
 function randomRGB() {
-    return `rgb(${randColorNum()},${randColorNum()},${randColorNum()})`;
+    return `rgb(${randomColorNum()},${randomColorNum()},${randomColorNum()})`;
 }
 
-// This function acts as the "switchboard"
+// This function acts as a "switchboard"
 function getColor() {
     if (drawingColor === "rainbow") {
         return randomRGB();
+    }
+    else if (drawingColor === "eraser") {
+        return "white";
     }
     return "black";
 }
@@ -66,4 +70,9 @@ function createGrid() {
         newDiv.style.height = basis; 
         gridContainer.appendChild(newDiv);
     }
+}
+
+clearButton.addEventListener("click", clearGrid);
+function clearGrid() {
+    gridContainer.innerHTML = "";
 }

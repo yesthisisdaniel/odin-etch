7090f36bab7ad63bc5e93a80ba68b49bd05d05e0
Daniel Nowak <danieln311@gmail.com>
2026-03-05 11:30:53 +0000

    Add 2 buttons to change color between black and rainbow, rework script to use less space and be less memory intensive.

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 const gridContainer = document.querySelector("#gridContainer")
-const button = document.querySelector(".button")
+const gridButton = document.querySelector("#gridButton")
+const rainbowButton = document.querySelector("#rainbowColor")
+const blackButton = document.querySelector("#blackColor")
 
-gridContainer.addEventListener("mousedown", draw)
-gridContainer.addEventListener("mousedown", (e) => {
-    e.preventDefault(); 
-
-    if (e.button === 0) {
-        isDrawing = true;
-        if (e.target.classList.contains("square")) {
-            e.target.style.backgroundColor = "orange";
-        }
-    }
-});
 
 let isDrawing = false;
+let drawingColor = "black"; 
+
 function randColorNum() {
     return Math.floor(Math.random() * 256);
 }
@@ -22,54 +15,55 @@ function randomRGB() {
     return `rgb(${randColorNum()},${randColorNum()},${randColorNum()})`;
 }
 
-function draw() {
-    gridContainer.addEventListener("mousedown", (e) => {
-        if (e.button === 0) {
-            isDrawing = true;
-            if (e.target.classList.contains("square")) {
-                e.target.style.backgroundColor = randomRGB();
-            }
-        }
-    })
-
-    gridContainer.addEventListener("mouseover", (e) => {
-        if (isDrawing && e.target.classList.contains("square")) {
-            e.target.style.backgroundColor = randomRGB();
-        }
-    });
-
-    const stopDrawing = () => {
-        isDrawing = false;
-    };
-
-    window.addEventListener("mouseup", stopDrawing);
-    window.addEventListener("blur", stopDrawing)
+// This function acts as the "switchboard"
+function getColor() {
+    if (drawingColor === "rainbow") {
+        return randomRGB();
+    }
+    return "black";
 }
 
-button.addEventListener("click", createGrid);
+gridContainer.addEventListener("mousedown", (e) => {
+    e.preventDefault(); 
+    if (e.button === 0) { // 0 is the code fo left click
+        isDrawing = true;
+        if (e.target.classList.contains("square")) {
+            e.target.style.backgroundColor = getColor();
+        }
+    }
+});
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (isDrawing && e.target.classList.contains("square")) {
+        e.target.style.backgroundColor = getColor();
+    }
+});
+
+const stopDrawing = () => isDrawing = false;
+window.addEventListener("mouseup", stopDrawing);
+window.addEventListener("blur", stopDrawing);
+
+blackButton.addEventListener("click", () => {
+    drawingColor = "black";
+});
+
+rainbowButton.addEventListener("click", () => {
+    drawingColor = "rainbow";
+});
+
+gridButton.addEventListener("click", createGrid);
 
 function createGrid() {
     gridContainer.innerHTML = "";
-
     let size = Number(prompt("What size grid are we making?"))
-
     if (size > 100) size = 100;
-
     const basis = (100 / size) + "%";
 
     for (let i = 0; i < size * size; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("square");
-
         newDiv.style.flexBasis = basis;
-
+        newDiv.style.height = basis; 
         gridContainer.appendChild(newDiv);
     }
-};
-
-
-
-
-
-
-
+}
